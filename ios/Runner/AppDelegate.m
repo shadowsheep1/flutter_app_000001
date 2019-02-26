a19b5ee6e5eb50919f2dfa7e7705e200f66ba612
Fabio Bombardi <shadowsheep@gmail.com>
2019-02-26 21:17:04 +0100

    .

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -6,6 +6,32 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+    FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+    
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"it.versionestabile.flutterapp000001/single"
+                                     binaryMessenger:controller];
+    
+    [channel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        
+        if ([call.method isEqualToString:@"crash"])
+        {
+            @throw([NSException
+                    exceptionWithName:@"iOS Exception"
+                    reason:@"Testing native iOS exceptions"
+                    userInfo:nil]);
+            
+            result(FlutterMethodNotImplemented);
+        }
+        else
+        {
+            result(FlutterMethodNotImplemented);
+        }
+        
+    }];
+    
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
